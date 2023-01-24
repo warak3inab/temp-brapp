e175053626f6934d54bcf0af35e52054b7b0613d
Abdulrahman Awad <abdul.hmha@gmail.com>
2023-01-24 12:50:30 +0000

    Changing anchor to button

--- src/components/About.js
+++ src/components/About.js
@@ -3,6 +3,12 @@ import aboutImg from '../images/about.jpeg'
 import {Title} from './Title'
 
 export const About = () => {
+
+  let btnFunc = () => {
+    // let mbtn = document.getElementById('btn')
+    document.documentElement.scrollTop = 0
+  }
+
   return (
     <section className="section" id="about">
       <Title title='About' subTitle='Us'/>
@@ -26,7 +32,8 @@ export const About = () => {
             quisquam harum nam cumque temporibus explicabo dolorum sapiente odio
             unde dolor?
           </p>
-          <a href="#" className="btn">read more</a>
+          {/* <a href="#" className="btn">read more</a> */}
+          <button type='button' onClick={btnFunc} className='btn'>read more</button>
         </article>
       </div>
     </section>
